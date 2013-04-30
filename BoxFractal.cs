c4bf4c8fc82599d14c5422aa402e4e09e577862f
William Andrus <andrus_wj@hotmail.com>
2013-04-30 11:33:39 -0600

    Refractor
    Optimize and made more readable.

--- BoxFractal.cs
+++ BoxFractal.cs
@@ -11,7 +11,7 @@ namespace FractalViewer
         private Color backgroundColor;
         private Color mainColor;
         private float precision;
-
+        private const float third = 0.33333F;
 
         /// <summary>
         /// Constructor
@@ -34,45 +34,41 @@ namespace FractalViewer
         /// <param name="iheight">Height of the control to draw on. </param>
         public void draw(int iwidth, int iheight)
         {
-            float third = 0.33333F;
-            //Create pens/brushes
-            Brush colorBrush = new SolidBrush(mainColor);
-            Pen colorPen = new Pen(colorBrush, 1.0F);
-            Brush backgroundBrush = new SolidBrush(backgroundColor);
-            Pen backgroundPen = new Pen(backgroundBrush, 1.0F);
+            using(Brush backgroundBrush = new SolidBrush(backgroundColor))
+            {
 
-            //Shift the upperLeft point to make room for tool strip at top
-            PointF upperLeft = new PointF((iwidth) * 0.04F, (iheight) * 0.04F);
-            //Shift height to fit inside.
-            float width = (iheight * 0.95F);
+                //Shift the upperLeft point to make room for tool strip at top
+                PointF upperLeft = new PointF((iwidth) * 0.04F, (iheight) * 0.04F);
+                //Shift height to fit inside.
+                float width = (iheight * 0.95F);
+                float thirdWidth = third * width;
+                float twoThirdWidth = 2 * thirdWidth;
 
-            //draw main block
-            g.FillRectangle(colorBrush, upperLeft.X, upperLeft.Y, width, width);
+                using (Brush colorBrush = new SolidBrush(mainColor))
+                {
+                    //draw main block
+                    g.FillRectangle(colorBrush, upperLeft.X, upperLeft.Y, width, width);
+                }
 
-            //sub divide the block by clearing top-middle, right-middle, bottom-middle, and left-middle.
-            PointF top = new PointF(upperLeft.X + width * third, upperLeft.Y);
-            g.FillRectangle(backgroundBrush, top.X, top.Y, width * third, width * third);
+                //sub divide the block by clearing top-middle, right-middle, bottom-middle, and left-middle.
+                PointF top = new PointF(upperLeft.X + thirdWidth, upperLeft.Y);
+                g.FillRectangle(backgroundBrush, top.X, top.Y, thirdWidth, thirdWidth);
 
-            PointF left = new PointF(upperLeft.X, upperLeft.Y + width * third);
-            g.FillRectangle(backgroundBrush, left.X, left.Y, width * third, width * third);
+                PointF left = new PointF(upperLeft.X, upperLeft.Y + thirdWidth);
+                g.FillRectangle(backgroundBrush, left.X, left.Y, thirdWidth, thirdWidth);
 
-            PointF right = new PointF(upperLeft.X + 2 * width * third, upperLeft.Y + width * third);
-            g.FillRectangle(backgroundBrush, right.X, right.Y, width * third, width * third);
+                PointF right = new PointF(upperLeft.X + twoThirdWidth, upperLeft.Y + thirdWidth);
+                g.FillRectangle(backgroundBrush, right.X, right.Y, thirdWidth, thirdWidth);
 
-            PointF bottom = new PointF(upperLeft.X + width * third, upperLeft.Y + 2 * width * third);
-            g.FillRectangle(backgroundBrush, bottom.X, bottom.Y, width * third, width * third);
+                PointF bottom = new PointF(upperLeft.X + thirdWidth, upperLeft.Y + twoThirdWidth);
+                g.FillRectangle(backgroundBrush, bottom.X, bottom.Y, thirdWidth, thirdWidth);
 
-            //Clean up
-            colorPen.Dispose();
-            backgroundPen.Dispose();
-            colorBrush.Dispose();
-            backgroundBrush.Dispose();
-
-            DrawBox(upperLeft, width * third);
-            DrawBox(new PointF(upperLeft.X + 2 * width * third, upperLeft.Y), width * third);
-            DrawBox(new PointF(upperLeft.X + width * third, upperLeft.Y + width * third), width * third);
-            DrawBox(new PointF(upperLeft.X, upperLeft.Y + 2 * width * third), width * third);
-            DrawBox(new PointF(upperLeft.X + 2 * width * third, upperLeft.Y + 2 * width * third), width * third);
+                DrawBox(backgroundBrush, upperLeft, thirdWidth);
+                DrawBox(backgroundBrush,new PointF(upperLeft.X + twoThirdWidth, upperLeft.Y), thirdWidth);
+                DrawBox(backgroundBrush, new PointF(upperLeft.X + thirdWidth, upperLeft.Y + thirdWidth), thirdWidth);
+                DrawBox(backgroundBrush, new PointF(upperLeft.X, upperLeft.Y + twoThirdWidth), thirdWidth);
+                DrawBox(backgroundBrush, new PointF(upperLeft.X + twoThirdWidth, upperLeft.Y + twoThirdWidth), thirdWidth);
+            }
         }
 
         /// <summary>
@@ -80,34 +76,32 @@ namespace FractalViewer
         /// </summary>
         /// <param name="upperLeft">The upper left of the block. </param>
         /// <param name="width">Represents the length. </param>
-        private void DrawBox(PointF upperLeft, float width)
+        private void DrawBox(Brush backgroundBrush, PointF upperLeft, float width)
         {
-            float third = 0.33333F;
-            if (width * third > this.precision)
+            float thirdWidth = width * third;
+            float twoThirdWidth = 2 * thirdWidth;
+
+            if (thirdWidth > this.precision)
             {
-                Brush backgroundBrush = new SolidBrush(backgroundColor);
+                    //Remove 4 more blocks
+                    PointF top = new PointF(upperLeft.X + thirdWidth, upperLeft.Y);
+                    this.g.FillRectangle(backgroundBrush, top.X, top.Y, thirdWidth, thirdWidth);
 
-                //Remove 4 more blocks
-                PointF top = new PointF(upperLeft.X + width * third, upperLeft.Y);
-                this.g.FillRectangle(backgroundBrush, top.X, top.Y, width * third, width * third);
+                    PointF left = new PointF(upperLeft.X, upperLeft.Y + thirdWidth);
+                    this.g.FillRectangle(backgroundBrush, left.X, left.Y, thirdWidth, thirdWidth);
 
-                PointF left = new PointF(upperLeft.X, upperLeft.Y + width * third);
-                this.g.FillRectangle(backgroundBrush, left.X, left.Y, width * third, width * third);
+                    PointF right = new PointF(upperLeft.X + twoThirdWidth, upperLeft.Y + thirdWidth);
+                    this.g.FillRectangle(backgroundBrush, right.X, right.Y, thirdWidth, thirdWidth);
 
-                PointF right = new PointF(upperLeft.X + 2 * width * third, upperLeft.Y + width * third);
-                this.g.FillRectangle(backgroundBrush, right.X, right.Y, width * third, width * third);
+                    PointF bottom = new PointF(upperLeft.X + thirdWidth, upperLeft.Y + twoThirdWidth);
+                    this.g.FillRectangle(backgroundBrush, bottom.X, bottom.Y, thirdWidth, thirdWidth);
 
-                PointF bottom = new PointF(upperLeft.X + width * third, upperLeft.Y + 2 * width * third);
-                this.g.FillRectangle(backgroundBrush, bottom.X, bottom.Y, width * third, width * third);
-
-                backgroundBrush.Dispose();
-
-                //Recursive into the 5 remaining.
-                DrawBox(upperLeft, width * third);
-                DrawBox(new PointF(upperLeft.X + 2 * width * third, upperLeft.Y), width * third);
-                DrawBox(new PointF(upperLeft.X + width * third, upperLeft.Y + width * third), width * third);
-                DrawBox(new PointF(upperLeft.X, upperLeft.Y + 2 * width * third), width * third);
-                DrawBox(new PointF(upperLeft.X + 2 * width * third, upperLeft.Y + 2 * width * third), width * third);
+                    //Recursive into the 5 remaining.
+                    DrawBox(backgroundBrush,upperLeft, thirdWidth);
+                    DrawBox(backgroundBrush, new PointF(upperLeft.X + twoThirdWidth, upperLeft.Y), thirdWidth);
+                    DrawBox(backgroundBrush, new PointF(upperLeft.X + thirdWidth, upperLeft.Y + thirdWidth), thirdWidth);
+                    DrawBox(backgroundBrush, new PointF(upperLeft.X, upperLeft.Y + twoThirdWidth), thirdWidth);
+                    DrawBox(backgroundBrush, new PointF(upperLeft.X + twoThirdWidth, upperLeft.Y + twoThirdWidth), thirdWidth);
             }
             else
             {
